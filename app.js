4f1ed4071f2c318969e8262938d6211a9d38719f
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-06-05 09:38:42 +0100

    Ch(user): install project dependencies
    - set up eslint and run the app
    [Delivers #166459043]

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-// import express from 'express';
-// import logger from 'morgan';
+import express from 'express';
+import logger from 'morgan';
 
-const express = require('express');
-const logger = require('morgan');
+// const express = require('express');
+// const logger = require('morgan');
+// var debug = require('debug')('http')
+//   , http = require('http')
+//   , name = 'My App';
+
+// debug('booting %o', name);
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,10 +15,10 @@ const port = process.env.PORT || 4000;
 app.use(logger('dev'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.get("", (req, res) => res.status(200).send({
-  message: "Welcome to AutoMart"
+app.get('/', (req, res) => res.status(200).send({
+  message: 'Welcome to AutoMart',
 }));
 
-app.listen(port, ()=>console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
